77f9b2401d0ae0b5101e7915185ee8b2ff238089
cristiandiazperdomo <danubiodiaz2003edf@gmail.com>
2023-01-24 21:09:06 -0300

    complete cellphone view

--- src/containers/Banner.jsx
+++ src/containers/Banner.jsx
@@ -8,19 +8,22 @@ const Banner = () => {
 			<div className="banner-right-side">
 				<img src={bannerImage} />
 			</div>
-			<div className="banner-left-side">
-				<h2>
-					Fod Fool
-				</h2>
-				<p>
-					COMPRA LOS MEJORES ALIMENTOS
-					PARA CUALQUIER ANIMAL, EN EL
-					MEJOR SITIO WEB DE MELO, CERRO
-					LARGO.
-				</p>
-				<button>
-					Mejores precios
-				</button>
+			<div className="banner-left-side-container">
+				<div className="banner-left-side">
+					<h2>
+						Fod Fool
+					</h2>
+					<p>
+						COMPRA LOS MEJORES ALIMENTOS
+						PARA CUALQUIER ANIMAL, EN EL
+						MEJOR SITIO WEB DE MELO, CERRO
+						LARGO.
+					</p>
+					<button>
+						Mejores precios
+					</button>
+				</div>
+				<div className="banner-border"></div>
 			</div>
 		</div>
 	)
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -6,7 +6,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, "dist"),
 	},
-	mode: 'production',
+	mode: 'development',
 	module: {
 		rules: [
 			{
